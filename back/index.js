18df4fa7143e0d300b128c4c129da30b30557ead
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2021-06-22 10:32:14 +0200

    fixed express static

--- back/index.js
+++ back/index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 
 const multer  = require('multer');
 const upload = multer({ dest: 'tmp/' });
